4490b3c6223535398147a634f7699540f5b59361
gabrieljimenezfg <gabrieljimenez.fg@gmail.com>
2025-10-18 18:58:10 +0200

    bouncer and level 2 wip

---
+++ Assets/Scripts/Bouncer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    [SerializeField] private int bounceForce = 1000;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var isPlayerCollision = collision.gameObject.CompareTag("Player");
+        if (!isPlayerCollision) return;
+        if (collision.GetContact(0).normal.y <= -0.5)
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce);
+        }
+    }
+}
